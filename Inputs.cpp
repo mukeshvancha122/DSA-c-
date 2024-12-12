4d6bfff9b8f3c8f7324398a0d764652755f81d90
Mukesh Reddy <mukeshvancha122@gmail.com>
2024-12-11 22:17:56 -0500

    arrays begining cpp

---
+++ Inputs.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter the value of a and b:"<<endl;
+    cin>>a>>b;
+    if(a>0 && b>0){
+        cout<<"A and B are positive"<<end;
+    }
+    else{
+        cout<<"A and B are negative"<<end;
+
+    }
+    return 0;
+}
---
+++ array/Array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int carprices[]={100,200,300,400};
+    int length = sizeof(carprices) / sizeof(carprices[0]);
+
+    for(int i=0;i<=length;i++){
+        cout<<carprices[i]<<" ";;
+    }
+
+    /**/  
+
+
+}
